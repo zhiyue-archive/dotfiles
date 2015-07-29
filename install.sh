b43b19d82f63119898b66bf6abd60f3781acc0d4
zhiyue <cszhiyue@gmail.com>
2015-07-30 02:29:45 +0800

    modified zsh

--- install.sh
+++ install.sh
@@ -17,29 +17,33 @@ git submodule update
 git submodule -q foreach git pull -q origin master
 
 
-# dir_colors 
+# dir_colors
 cd $CURRENT_DIR/config/dircolors-solarized/
-lnif $CURRENT_DIR/config/dircolors-solarized/dircolors.256dark $HOME/.dir_colors
-eval `dircolors dircolors.256dark`
-echo "export TERM=xterm-256color">>$HOME/.bashrc
+lnif $CURRENT_DIR/config/dircolors-solarized/dircolors.256dark $HOME/.dircolors
+#eval `dircolors dircolors.256dark`
+#echo "export TERM=xterm-256color">>$HOME/.bashrc
 #source $HOME/.bashrc
 
 
-# change terminal theme to solarized
-cd $CURRENT_DIR/config/gnome-terminal-colors-solarized/
-./set_dark.sh
 
-
-# set up zsh 
+# set up zsh
 
 ## 1. use oh-my-zsh
 ## 2. link .zshrc
 ## 3. link theme
+system_shell=$SHELL
+export SHELL="/bin/zsh"
+#git clone git://github.com/zsh-users/antigen.git ${HOME}/.antigen
+lnif $CURRENT_DIR/zsh/antigen $HOME/.antigen
 lnif $CURRENT_DIR/zsh/oh-my-zsh $HOME/.oh-my-zsh
 lnif $CURRENT_DIR/zsh/zshrc $HOME/.zshrc
 lnif $CURRENT_DIR/zsh/zhiyue.zsh-theme $HOME/.oh-my-zsh/themes/zhiyue.zsh-theme
+source $HOME/.zshrc
+export SHELL=$system_shell 
 
-#source $HOME/.zshrc
+# change terminal theme to solarized
+cd $CURRENT_DIR/config/gnome-terminal-colors-solarized/
+./set_dark.sh
 
 
 #Copy tmux config to home:
